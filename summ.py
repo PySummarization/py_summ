ca0e8258e375cf67c90366e0a9faef4e7e8d2445
Pablo Vinicius <vinicius.pablo.18@gmail.com>
2016-10-03 22:21:53 -0300

    added comments

--- summ.py
+++ summ.py
@@ -6,16 +6,16 @@ def read_file():
 
 text = read_file()
 
+#getting the tokens without pontuation
 tokenizer = nltk.tokenize.RegexpTokenizer(r'\w+')
 tokens = tokenizer.tokenize(text.read())
 
-
+#removing the stopwords and generating a list with most common words
 stopwords = nltk.corpus.stopwords.words("portuguese")
 freq2 = nltk.FreqDist(w for w in tokens if w not in stopwords)
 commons = freq2.most_common(1000)
 
-
-
+#generating a list with the five (or more) more common words
 usefull_list = []
 i = 0
 for n in commons:
