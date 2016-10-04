6421d8f6a7fee72d149da1fc4dc5046b573d831d
Pablo Vinicius <vinicius.pablo.18@gmail.com>
2016-10-03 22:51:32 -0300

    generating sentences with the more common words

--- summ.py
+++ summ.py
@@ -4,11 +4,11 @@ def read_file():
     read = open("example.txt", "r")
     return read
 
-text = read_file()
+text = read_file().read()
 
 #getting the tokens without pontuation
 tokenizer = nltk.tokenize.RegexpTokenizer(r'\w+')
-tokens = tokenizer.tokenize(text.read())
+tokens = tokenizer.tokenize(text)
 
 #removing the stopwords and generating a list with most common words
 stopwords = nltk.corpus.stopwords.words("portuguese")
@@ -22,3 +22,14 @@ for n in commons:
     usefull_list.append(n[0])
     i += 1
     if i == 5: break
+
+print(usefull_list)
+
+
+#generating a list with the sentences that the more common words appears
+expressions = []
+for n in usefull_list:
+    regex = '[\w," ]+' + n + '[\w," ]+\.'
+    tokenizer = nltk.tokenize.RegexpTokenizer(regex)
+    tokens = tokenizer.tokenize(text)
+    for i in tokens: expressions.append(i)
