d60b24e8c4158e52efad919401743b5a4d1a652c
Pablo Vinicius <vinicius.pablo.18@gmail.com>
2016-10-03 21:57:52 -0300

    removed stopwords from tokens

--- summ.py
+++ summ.py
@@ -8,3 +8,8 @@ text = read_file()
 
 tokenizer = nltk.tokenize.RegexpTokenizer(r'\w+')
 tokens = tokenizer.tokenize(text.read())
+
+
+stopwords = nltk.corpus.stopwords.words("portuguese")
+freq2 = nltk.FreqDist(w for w in tokens if w not in stopwords)
+print(freq2.most_common(1000))
