4948a2455094bb85de83fbc444bc6d65ec9131e1
Artur Miranda <ldm.artur@gmail.com>
2016-10-05 10:55:27 -0300

    Add sentence regex

--- summ.py
+++ summ.py
@@ -15,39 +15,42 @@ stopwords = nltk.corpus.stopwords.words("portuguese")
 freq2 = nltk.FreqDist(w for w in tokens if w not in stopwords)
 commons = freq2.most_common(1000)
 
-#generating a list with the five (or more) more common words
-usefull_list = []
-i = 0
-for n in commons:
-    usefull_list.append(n[0])
-    i += 1
-    if i == 5: break
-
-#print(usefull_list)
+sentence_regex = "([A-Z][^\.!?]*[\.!?])"
 
 
-#generating a list with the sentences that the more common words appears
-expressions = []
-for n in usefull_list:
-    regex = '[\w," ]+' + n + '[\w," ]+\.'
-    tokenizer = nltk.tokenize.RegexpTokenizer(regex)
-    tokens = tokenizer.tokenize(text)
-    for i in tokens: expressions.append(i)
+# #generating a list with the five (or more) more common words
+# usefull_list = []
+# i = 0
+# for n in commons:
+#     usefull_list.append(n[0])
+#     i += 1
+#     if i == 5: break
 
-#excluding repeated expressions
-def exclude_repeated_sentences(sentences):
-    not_repeated = []
-    not_repeated.append(expressions[0])
-    flag = 1
-
-    for n in sentences:
-        for i in not_repeated:
-            if n == i: flag = 0
-        if flag: not_repeated.append(n)
-        else: flag = 1
-
-    return not_repeated
+# #print(usefull_list)
 
 
-for i in exclude_repeated_sentences(expressions):
-    print(i + "\n")
+# #generating a list with the sentences that the more common words appears
+# expressions = []
+# for n in usefull_list:
+#     regex = '[\w," ]+' + n + '[\w," ]+\.'
+#     tokenizer = nltk.tokenize.RegexpTokenizer(regex)
+#     tokens = tokenizer.tokenize(text)
+#     for i in tokens: expressions.append(i)
+
+# #excluding repeated expressions
+# def exclude_repeated_sentences(sentences):
+#     not_repeated = []
+#     not_repeated.append(expressions[0])
+#     flag = 1
+
+#     for n in sentences:
+#         for i in not_repeated:
+#             if n == i: flag = 0
+#         if flag: not_repeated.append(n)
+#         else: flag = 1
+
+#     return not_repeated
+
+
+# for i in exclude_repeated_sentences(expressions):
+#     print(i + "\n")
