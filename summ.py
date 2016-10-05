876c44873d0c0df0a4f3abf850f4107767701786
Pablo Vinicius <vinicius.pablo.18@gmail.com>
2016-10-05 12:15:32 -0300

    massive alterations :D

--- summ.py
+++ summ.py
@@ -1,7 +1,7 @@
-import nltk
+import nltk, re
 
 def read_file():
-    read = open("example.txt", "r")
+    read = open("example.txt", "r", encoding='utf8')
     return read
 
 text = read_file().read()
@@ -15,7 +15,40 @@ stopwords = nltk.corpus.stopwords.words("portuguese")
 freq2 = nltk.FreqDist(w for w in tokens if w not in stopwords)
 commons = freq2.most_common(1000)
 
-sentence_regex = "([A-Z][^\.!?]*[\.!?])"
+sentence_regex = u"([^a-z\s][^\.!?]*[\.!?])"
+
+matchObj = re.findall(sentence_regex, text)
+
+tuple_list = []
+
+def word_score(word):
+    for i in commons:
+        if i[0] == word:
+            return i[1]
+    return 0
+
+def score(sent):
+    score = 0
+    tokenizer = nltk.tokenize.RegexpTokenizer(r'\w+')
+    tokens = tokenizer.tokenize(sent)
+    for i in tokens:
+        score += word_score(i)
+    return score
+
+
+for i in matchObj:
+    tup = (i,score(i))
+    tuple_list.append(tup)
+
+sort_one = sorted(tuple_list,key=lambda x: x[1], reverse=True)
+
+def print_results(sorteds):
+    print('SENTENCE SCORES\n')
+    for i in sorteds:
+        print('\nSentence: ',  i[0])
+        print('Score: ', i[1])
+
+print_results(sort_one)
 
 
 # #generating a list with the five (or more) more common words
